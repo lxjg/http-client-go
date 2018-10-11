e8d14ffb64b2981fd48888b0c2320cb9404b3ea0
gitWhailon <whl519062@gmail.com>
2018-10-11 17:00:21 +0800

    Add set request header

--- main.go
+++ main.go
@@ -5,9 +5,42 @@ import (
 	"net/http"
 )
 
+type HttpClient struct {
+	Header map[string]string
+}
+
+func NewHttpClient() *HttpClient {
+	return &HttpClient{}
+}
+
+func (c *HttpClient) SetHeader(k string, v string) *HttpClient {
+	if c.Header == nil {
+		c.Header = make(map[string]string)
+	}
+	c.Header[k] = v
+
+	return c
+}
+
+func (c *HttpClient) SetJWTAuth(token string) *HttpClient {
+	return c.SetHeader("Authorization", token)
+}
+
 // Get 请求
-func Get(requestLine string) ([]byte, error) {
-	response, err := http.Get(requestLine)
+func (c *HttpClient) Get(requestLine string) ([]byte, error) {
+	req, err := http.NewRequest("GET", requestLine, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.Header != nil {
+		for k, v := range c.Header {
+			req.Header.Set(k, v)
+		}
+	}
+
+	client := http.Client{}
+	response, err := client.Do(req)
 	if err != nil || response.StatusCode != http.StatusOK {
 		return nil, err
 	}
